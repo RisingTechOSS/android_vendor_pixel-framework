92eff3de9216eff8825651659910ef2fb2bc7e87
Asriadi Rahim <asriadirahim03@gmail.com>
2024-07-10 01:20:03 +0800

    pwf: Remove elmyra stub * Elmyra from Google causes the OS to become corrupted on legacy devices such as the   Pixel 2 series and prevents the OS from loading properly,   we can use from LineageOS as an alternative * And who really needs to squish their device for Google Assistant
    Log: https://katb.in/emafuyireqi
    Test: Build & Boot
    
    Signed-off-by: Asriadi Rahim <asriadirahim03@gmail.com>

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
@@ -123,7 +123,6 @@ import com.google.android.systemui.assist.AssistManagerGoogle;
 import com.google.android.systemui.assist.dagger.AssistModule;
 import com.google.android.systemui.columbus.dagger.ColumbusModule;
 import com.google.android.systemui.controls.GoogleControlsTileResourceConfigurationImpl;
-import com.google.android.systemui.elmyra.dagger.ElmyraModule;
 import com.google.android.systemui.dreamliner.DockObserver;
 import com.google.android.systemui.dreamliner.dagger.DreamlinerModule;
 import com.google.android.systemui.power.dagger.PowerModuleGoogle;
@@ -137,7 +136,6 @@ import com.google.android.systemui.smartspace.dagger.SmartspaceGoogleModule;
 import com.google.android.systemui.statusbar.dagger.StartCentralSurfacesGoogleModule;
 import com.google.android.systemui.statusbar.KeyguardIndicationControllerGoogle;
 import com.google.android.systemui.statusbar.policy.BatteryControllerImplGoogle;
-import com.google.android.systemui.elmyra.ServiceConfigurationGoogle;
 import com.google.android.systemui.statusbar.policy.dagger.SystemUIGooglePolicyModule;
 import com.google.android.systemui.SystemUIGoogleAppComponentFactory;
 
@@ -183,7 +181,6 @@ import dagger.Lazy;
         DreamlinerModule.class,
         ReverseChargingModule.class,
         AssistModule.class,
-        ElmyraModule.class,
         ColumbusModule.class,
         SystemUIGooglePolicyModule.class,
         LineageModule.class
--- SystemUIGoogle/src/com/google/android/systemui/dreamliner/DockObserver.java
+++ SystemUIGoogle/src/com/google/android/systemui/dreamliner/DockObserver.java
@@ -62,7 +62,6 @@ import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.google.android.systemui.dreamliner.WirelessCharger;
-import com.google.android.systemui.elmyra.gates.KeyguardVisibility;
 
 import dagger.Lazy;
 
@@ -495,7 +494,7 @@ public class DockObserver extends DockManagerImpl {
             intent.putExtra("type", i);
             intent.putExtra("orientation", i2);
             intent.putExtra("id", i3);
-            intent.putExtra("occluded", new KeyguardVisibility(context).isKeyguardOccluded());
+            intent.putExtra("occluded", false);
             try {
                 DreamlinerServiceConn dreamlinerServiceConn = new DreamlinerServiceConn(context);
                 mDreamlinerServiceConn = dreamlinerServiceConn;
